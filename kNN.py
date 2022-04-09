e7fb644ff659ebe198e7f60adc312fe7dea466fb
Gulce62 <98024902+Gulce62@users.noreply.github.com>
2022-04-09 11:50:57 +0300

    Link knn algorithm to the dataset and separate sound features

--- kNN.py
+++ kNN.py
@@ -1,5 +1,5 @@
 import numpy as np
-from collections import Counter
+from statistics import mode
 
 
 def calculate_euclidean_distance(a, b):
@@ -12,10 +12,10 @@ def calculate_euclidean_distance(a, b):
 def calculate_manhattan_distance(a, b):
     a = np.array(a)
     b = np.array(b)
+    size = len(a)
     man_distance = 0
-    for i in a:
-        for j in b:
-            man_distance += abs(a-b)
+    for i in range(size):
+        man_distance += abs(a[i]-b[i])
     return man_distance
 
 
@@ -24,12 +24,12 @@ def calculate_cosine_distance(a, b):
     return cos_distance
 
 
-class KNN:
-    def __init__(self, k=3, p=1):
+class KNN():
+    def __init__(self, k=3, distance_type=1):
         self.X_train = None
         self.y_train = None
         self.k = k
-        self.p = p
+        self.distance_type = distance_type
 
     def fit(self, X_train, y_train):
         self.X_train = X_train
@@ -38,9 +38,9 @@ class KNN:
     def emotion_predict(self, X_test):
         a_distance = np.array([])
         for X in self.X_train:
-            if self.p == 1:
+            if self.distance_type == 1:
                 distance = calculate_euclidean_distance(X_test, X)
-            elif self.p == 2:
+            elif self.distance_type == 2:
                 distance = calculate_manhattan_distance(X_test, X)
             else:
                 distance = calculate_cosine_distance(X_test, X)
@@ -49,11 +49,11 @@ class KNN:
         a_knn_neighbors = np.array([])
         for index in knn_indices:
             a_knn_neighbors = np.append(a_knn_neighbors, self.y_train[index])
-        return Counter(a_knn_neighbors).most_common(1)[0][0]
+        return mode(a_knn_neighbors)
 
-    def all_prediction(self):
+    def all_prediction(self, X_train):
         a_emotions_predictions = np.array([])
-        for X in self.X_train:
+        for X in X_train:
             prediction = self.emotion_predict(X)
             a_emotions_predictions = np.append(a_emotions_predictions, prediction)
         return a_emotions_predictions
--- main.py
+++ main.py
@@ -1,75 +1,16 @@
-import librosa
 import numpy as np
 import pandas as pd
-import soundfile as sf
-import librosa.display
-import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
-import os
+from sklearn.model_selection import train_test_split
+from soundFeatures import get_feature_lists
+from kNN import KNN
 
 import warnings
 
 warnings.filterwarnings('ignore')
 
-
-# Modality (01 = full-AV, 02 = video-only, 03 = audio-only) --> Always 03
-# Vocal channel (01 = speech, 02 = song) --> Always 01
-# Emotion (01 = neutral, 02 = calm, 03 = happy, 04 = sad, 05 = angry, 06 = fearful, 07 = disgust, 08 = surprised).
-# Emotional intensity (01 = normal, 02 = strong). NOTE: There is no strong intensity for the 'neutral' emotion.
-# Statement (01 = "Kids are talking by the door", 02 = "Dogs are sitting by the door").
-# Repetition (01 = 1st repetition, 02 = 2nd repetition).
-# Actor (01 to 24. Odd numbered actors are male, even numbered actors are female).
-
-
-def sound_features(sound_path):
-    data, sampling_rate = sf.read(sound_path, dtype='float32')
-    chroma = chroma_feature(data, sampling_rate)
-    mfcc = mfcc_feature(data, sampling_rate)
-    mel = mel_feature(data, sampling_rate)
-    sound_feature_matrix = np.concatenate((chroma, mel, mfcc), axis=0, out=None)
-    return sound_feature_matrix
-
-
-def chroma_feature(data, sampling_rate):
-    stft_data = librosa.stft(data)
-    stft_data_abs = np.abs(stft_data)
-    chroma_data = librosa.feature.chroma_stft(S=stft_data_abs, sr=sampling_rate)
-    chroma_mean = np.mean(chroma_data.T, axis=0)
-    return chroma_mean
-
-
-def mfcc_feature(data, sampling_rate):
-    mfcc_data = librosa.feature.mfcc(data, sampling_rate, n_mfcc=40)
-    mfcc_mean = np.mean(mfcc_data.T, axis=0)
-    return mfcc_mean
-
-
-def mel_feature(data, sampling_rate):
-    mel_data = librosa.feature.melspectrogram(data, sampling_rate, n_mels=128, fmax=8000)
-    mel_mean = np.mean(mel_data.T, axis=0)
-    return mel_mean
-
-
-def get_feature_lists():
-    emotion_list = []
-    sound_features_list = []
-    audio_main_path = "/Users/a/Desktop/Dataset/"
-    actor_folders = os.listdir(audio_main_path)
-    for actor in actor_folders:
-        actor_path = audio_main_path + actor
-        audio_files = os.listdir(actor_path)
-        for audio_file in audio_files:
-            file_name = audio_file.strip().split('.')
-            file_properties = file_name[0].strip().split('-')
-            emotion = file_properties[2]
-            file_path = actor_path + '/' + audio_file
-            sound_feature = sound_features(file_path)
-            print(audio_file)
-            emotion_list.append(emotion)
-            sound_features_list.append(sound_feature)
-        print(actor)
-        return emotion_list, sound_features_list
-
+audio_main_path = "/Users/a/Desktop/Dataset/"
+[emotion_list, sound_features_list] = get_feature_lists(audio_main_path)
 
 emotion_dict = {'01': 'neutral',
                 '02': 'calm',
@@ -81,7 +22,6 @@ emotion_dict = {'01': 'neutral',
                 '08': 'surprised'
                 }
 
-[emotion_list, sound_features_list] = get_feature_lists()
 emotion_dataframe = pd.DataFrame(emotion_list)
 emotion_dataframe = emotion_dataframe.replace(emotion_dict)
 features_dataframe = pd.DataFrame(sound_features_list)
@@ -89,23 +29,55 @@ main_dataframe = pd.concat([emotion_dataframe, features_dataframe], axis=1)
 
 main_dataframe.to_csv('/Users/a/Desktop/audio.csv')
 
-# from sklearn.preprocessing import StandardScaler
-# scalerValue = StandardScaler()
-# #Preserving the unscaled values to process them if something
-# happens
-# featuresScaled = features_of_sound
-# featuresScaled = scalerValue.fit_transform(featuresScaled)
-# from sklearn.model_selection import train_test_split
-# #Unscaled train/test set is as follows:
-# Xtrain, Xtest, ytrain, ytest = train_test_split(
-# features_of_sound,
-# emotions,
-# testSize = 0.1,
-# randomState = 1088
-# )
-# #Scaled train/test set is as follows:
-# Xtrain_scaled, Xtest_scaled, ytrain, ytest = train_test_split(
-# featuresScaled,
-# emotions,
-# testSize = 0.1,
-# randomState = 1088
+scaling_value = StandardScaler()
+scaled_features = sound_features_list
+scaled_features = scaling_value.fit_transform(scaled_features)
+emotion_features = np.array(emotion_list)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    scaled_features,
+    emotion_list,
+    test_size=0.2,
+    random_state=50
+)
+
+X_train, X_val, y_train, y_val = train_test_split(
+    X_train,
+    y_train,
+    test_size=0.25,
+    random_state=50
+)
+
+print("X_train shape: {}".format(X_train.shape))
+print("X_test shape: {}".format(X_test.shape))
+print("X_val shape: {}".format(X_val.shape))
+print("y_train shape: {}".format(np.array(y_train).shape))
+print("y_test shape: {}".format(np.array(y_test).shape))
+print("y_val shape: {}".format(np.array(y_val).shape))
+
+
+def calculate_accuracy(true_value, prediction_value):
+    size = len(true_value)
+    sum_val = 0
+    print(size)
+    for item in range(size):
+        if true_value[item] == prediction_value[item]:
+            sum_val += 1
+    print(sum_val)
+    accuracy_percentage = sum_val / size * 100
+    return accuracy_percentage
+
+
+distance_type = ['euclidian distance', 'manhattan distance', 'cosine distance']
+# for d_type in range(1, 4):
+#     classify_knn = KNN(k=3, distance_type=d_type)
+#     classify_knn.fit(X_train, y_train)
+#     y_prediction = classify_knn.all_prediction(X_train)
+#     print("Accuracy Percentage of the KNN classification with", distance_type[d_type-1],
+#           "is", calculate_accuracy(y_train, y_prediction))
+
+classify_knn = KNN(k=3, distance_type=2)
+classify_knn.fit(X_train, y_train)
+y_prediction = classify_knn.all_prediction(X_train)
+print("Accuracy Percentage of the KNN classification with", distance_type[2],
+          "is", calculate_accuracy(y_train, y_prediction))
