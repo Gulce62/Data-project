fc8e5d9cf72ab8fdbe4a097a2c75eaad23a79110
Gulce62 <98024902+Gulce62@users.noreply.github.com>
2022-04-26 17:40:30 +0300

    Added validation for kNN

--- main.py
+++ main.py
@@ -34,19 +34,22 @@ scaled_features = sound_features_list
 scaled_features = scaling_value.fit_transform(scaled_features)
 emotion_features = np.array(emotion_list)
 
+# TODO make cross validation for finding the percentage of test train validation datasets
 X_train, X_test, y_train, y_test = train_test_split(
     scaled_features,
     emotion_list,
-    test_size=0.2,
-    random_state=20
+    test_size=0.1,
+    random_state=1
 )
 
 X_train, X_val, y_train, y_val = train_test_split(
     X_train,
     y_train,
-    test_size=0.25,
-    random_state=20
+    test_size=1/9,
+    random_state=1
 )
+
+# TODO look for reshape necessity
 
 print("X_train shape: {}".format(X_train.shape))
 print("X_test shape: {}".format(X_test.shape))
@@ -67,13 +70,36 @@ def calculate_accuracy(true_value, prediction_value):
     return accuracy_percentage
 
 
+# distance_type = ['euclidian distance', 'manhattan distance', 'cosine distance']
+# for d_type in range(1, 4):
+#     classify_knn = KNN(k=3, distance_type=d_type)
+#     classify_knn.fit(X_train, y_train)
+#     y_prediction_train = classify_knn.all_prediction(X_train)
+#     y_prediction_test = classify_knn.all_prediction(X_test)
+#     print("Accuracy Percentage of KNN classification for train data with",
+#           distance_type[d_type - 1], "is", calculate_accuracy(y_train, y_prediction_train))
+#     print("Accuracy Percentage of KNN classification for test data with",
+#           distance_type[d_type - 1], "is", calculate_accuracy(y_test, y_prediction_test))
+
+accuracy = 0
+optimal_k = None
+optimal_d_type = None
 distance_type = ['euclidian distance', 'manhattan distance', 'cosine distance']
 for d_type in range(1, 4):
-    classify_knn = KNN(k=3, distance_type=d_type)
-    classify_knn.fit(X_train, y_train)
-    y_prediction_train = classify_knn.all_prediction(X_train)
-    y_prediction_test = classify_knn.all_prediction(X_test)
-    print("Accuracy Percentage of KNN classification for train data with",
-          distance_type[d_type - 1], "is", calculate_accuracy(y_train, y_prediction_train))
-    print("Accuracy Percentage of KNN classification for test data with",
-          distance_type[d_type - 1], "is", calculate_accuracy(y_test, y_prediction_test))
+    for k_val in range(1, 10):
+        validate_knn = KNN(k=k_val, distance_type=d_type)
+        validate_knn.fit(X_train, y_train)
+        y_prediction_val = validate_knn.all_prediction(X_val)
+        val_accuracy = calculate_accuracy(y_val, y_prediction_val)
+        print("Accuracy Percentage of KNN classification for test data with",
+              distance_type[d_type - 1], "and k =", k_val, "is", val_accuracy)
+        if val_accuracy >= accuracy:
+            accuracy = val_accuracy
+            optimal_k = k_val
+            optimal_d_type = d_type
+print(optimal_k)
+print(optimal_d_type)
+
+# TODO ask if the accuracy same
+# TODO create confusion matrix
+# TODO calculate mean squared error
