87ba8f2866d0eaf107b3cc6b829b3d0b7f46df4f
Gulce62 <98024902+Gulce62@users.noreply.github.com>
2022-04-29 18:53:06 +0300

    implement SVM algorithm

--- SVM.py
+++ SVM.py
@@ -17,15 +17,62 @@ def calculate_gaussian_kernel(a, b, sigma):
 
 
 class SVM:
-    def __init__(self, kernel='gaussian', regularization_parameter_C=100):
+    def __init__(self, kernel='gaussian', regularization_parameter_C=100, max_iterations=60):
         self.X_train = None
         self.y_train = None
+        self.matrix_K = None
+        self.support_vector = None
+        self.alpha_variables = None
+        self.bias = None
         self.kernel = {'linear': lambda x, y: calculate_linear_kernel(x, y),
                        'polynomial_deg2': lambda x, y: calculate_polynomial_kernel(x, y, 0, 2),
                        'polynomial_deg3': lambda x, y: calculate_polynomial_kernel(x, y, 0, 3),
                        'gaussian': lambda x, y: calculate_gaussian_kernel(x, y, 1)}[kernel]
         self.regularization_parameter_C = regularization_parameter_C
+        self.max_iterations = max_iterations
 
     def fit(self, X_train, y_train):
-        self.X_train = X_train
-        self.y_train = y_train
+        self.X_train = X_train.copy()
+        self.y_train = y_train * 2 - 1
+        kernel_matrix = self.kernel(self.X_train, self.X_train)
+        y_matrix = self.y_train * self.y_train[:, np.newaxis]
+        threshold = 1e-15
+        self.matrix_K = kernel_matrix * y_matrix
+        self.alpha_variables = self.find_alphas(y_train, self.max_iterations, threshold)
+        self.bias = self.get_bias(threshold)
+
+    def find_alphas(self, y_train, max_iterations, threshold):
+        (row, column) = y_train.shape
+        alpha_variables = np.zeros((row, column), dtype=float)
+        while max_iterations != 0:
+            length = len(alpha_variables)
+            for index_m in range(length):
+                index_l = np.random.randint(0, length)
+                main_index = [index_m, index_l]
+                q_parameter = self.matrix_K[[[index_m, index_m], [index_l, index_l]], [main_index, [index_l, index_m]]]
+                v0_parameter = alpha_variables[main_index]
+                m_l_indices = alpha_variables * self.matrix_K[main_index]
+                k0_parameter = 1 - np.sum(m_l_indices, axis=1)
+                u_value = np.array(-self.y_train[index_l], self.y_train[index_m])
+                maximum_t = np.dot(k0_parameter, u_value) / (np.dot(np.dot(q_parameter, u_value), u_value) + threshold)
+                v_function = v0_parameter + u_value * maximum_t
+                if maximum_t[0] or maximum_t[1] > self.regularization_parameter_C:
+                    t_res = (np.clip(v_function, 0, self.regularization_parameter_C))[1] / u_value[1]
+                    new_v_function = v0_parameter + u_value * t_res
+                    t_res = (np.clip(new_v_function, 0, self.regularization_parameter_C))[0] / u_value[0]
+                    v_function = v0_parameter + u_value * t_res
+                alpha_variables[main_index] = v_function
+        return alpha_variables
+
+    def get_bias(self, threshold):
+        non_zero_indices = np.nonzero(self.alpha_variables > threshold)
+        summation = np.sum(self.matrix_K[non_zero_indices] * self.alpha_variables, axis=1)
+        bias = np.mean((1.0 - summation) * self.y_train[non_zero_indices])
+        return bias
+
+    def emotion_predict(self, X_train):
+        new_kernel_matrix = self.kernel(X_train, self.X_train)
+        new_y_matrix = self.y_train * self.alpha_variables
+        support_vector = np.sum(new_kernel_matrix * new_y_matrix, axis=1) + self.bias
+        sign_prediction = (np.sign(support_vector + 1)) // 2
+        return sign_prediction
---
+++ main_SVM.py
@@ -0,0 +1,91 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from soundFeatures import get_feature_lists
+from svm import SVM
+
+import warnings
+
+warnings.filterwarnings('ignore')
+
+audio_main_path = "/Users/a/Desktop/Dataset/"
+[emotion_list, sound_features_list] = get_feature_lists(audio_main_path)
+
+emotion_dict = {'01': 'neutral',
+                '02': 'calm',
+                '03': 'happy',
+                '04': 'sad',
+                '05': 'angry',
+                '06': 'fearful',
+                '07': 'disgust',
+                '08': 'surprised'
+                }
+
+emotion_dataframe = pd.DataFrame(emotion_list)
+emotion_dataframe = emotion_dataframe.replace(emotion_dict)
+features_dataframe = pd.DataFrame(sound_features_list)
+main_dataframe = pd.concat([emotion_dataframe, features_dataframe], axis=1)
+
+main_dataframe.to_csv('/Users/a/Desktop/audio.csv')
+
+scaling_value = StandardScaler()
+scaled_features = sound_features_list
+scaled_features = scaling_value.fit_transform(scaled_features)
+emotion_features = np.array(emotion_list)
+
+# TODO make cross validation for finding the percentage of test train validation datasets
+X_train, X_test, y_train, y_test = train_test_split(
+    scaled_features,
+    emotion_list,
+    test_size=0.1,
+    random_state=1
+)
+
+X_train, X_val, y_train, y_val = train_test_split(
+    X_train,
+    y_train,
+    test_size=1 / 9,
+    random_state=1
+)
+
+# TODO look for reshape necessity
+
+print("X_train shape: {}".format(X_train.shape))
+print("X_test shape: {}".format(X_test.shape))
+print("X_val shape: {}".format(X_val.shape))
+print("y_train shape: {}".format(np.array(y_train).shape))
+print("y_test shape: {}".format(np.array(y_test).shape))
+print("y_val shape: {}".format(np.array(y_val).shape))
+
+
+def calculate_accuracy(true_value, prediction_value):
+    size = len(true_value)
+    sum_val = 0
+    for item in range(size):
+        if true_value[item] == prediction_value[item]:
+            sum_val += 1
+    print(sum_val, 'values are predicted true from', size)
+    accuracy_percentage = sum_val / size * 100
+    return accuracy_percentage
+
+
+accuracy = 0
+
+cList = [0.0001, 0.001, 0.01, 0.1, 1, 10, 100, 1000, 10000, 100000]
+kernelList = ["linear", "polynomial", "gaussian"]
+
+svmClassifier = SVM(kernel=kernelList[0], regularization_parameter_C=cList[5])
+
+# disgust training data (1)
+yDisgustTrain = np.where(y_train == "disgust", 1, -1)
+svmClassifier.fit(X_train, yDisgustTrain)
+s = svmClassifier.emotion_predict(X_test)
+# prediction making
+yTest = np.where(s == 1, "disgust", s)
+# disgust test data
+
+print("SVM classification accuracy", " kernel parameter=",
+      kernelList[0], " C parameter=", cList[5],
+      calculate_accuracy(y_test, yTest))
+
