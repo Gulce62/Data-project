1a2dd29d5f6ea8d894be578fe0a1f9aa9a14d9e8
Gulce62 <98024902+Gulce62@users.noreply.github.com>
2022-05-06 17:29:02 +0300

    Finalized kNN

--- main_kNN.py
+++ main_kNN.py
@@ -2,51 +2,34 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-from soundFeatures import get_feature_lists
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
 from kNN import KNN
 
-import warnings
-
-warnings.filterwarnings('ignore')
-
-audio_main_path = "/Users/a/Desktop/Dataset/"
-[emotion_list, sound_features_list] = get_feature_lists(audio_main_path)
-
-emotion_dict = {'01': 'neutral',
-                '02': 'calm',
-                '03': 'happy',
-                '04': 'sad',
-                '05': 'angry',
-                '06': 'fearful',
-                '07': 'disgust',
-                '08': 'surprised'
-                }
-
-emotion_dataframe = pd.DataFrame(emotion_list)
-emotion_dataframe = emotion_dataframe.replace(emotion_dict)
-features_dataframe = pd.DataFrame(sound_features_list)
-main_dataframe = pd.concat([emotion_dataframe, features_dataframe], axis=1)
-
-main_dataframe.to_csv('/Users/a/Desktop/audio.csv')
+emotions_dataframe = pd.read_csv('/Users/a/Desktop/emotions.csv', dtype=str)
+features_dataframe = pd.read_csv('/Users/a/Desktop/features.csv')
+features_dataframe = features_dataframe.iloc[:, 1:]
+emotion_list = emotions_dataframe['0'].values.tolist()
+sound_features_list = features_dataframe.values.tolist()
 
 scaling_value = StandardScaler()
 scaled_features = sound_features_list
 scaled_features = scaling_value.fit_transform(scaled_features)
-emotion_features = np.array(emotion_list)
 
 # TODO make cross validation for finding the percentage of test train validation datasets
 X_train, X_test, y_train, y_test = train_test_split(
     scaled_features,
     emotion_list,
-    test_size=0.1,
-    random_state=1
+    test_size=0.2,
+    random_state=60
 )
 
 X_train, X_val, y_train, y_val = train_test_split(
     X_train,
     y_train,
-    test_size=1/9,
-    random_state=1
+    test_size=0.25,
+    random_state=60
 )
 
 # TODO look for reshape necessity
@@ -75,7 +58,7 @@ optimal_k = None
 optimal_d_type = None
 distance_type = ['euclidian distance', 'manhattan distance', 'cosine distance']
 for d_type in range(1, 4):
-    for k_val in range(1, 10):
+    for k_val in range(1, 11):
         validate_knn = KNN(k=k_val, distance_type=d_type)
         validate_knn.fit(X_train, y_train)
         y_prediction_val = validate_knn.all_prediction(X_val)
@@ -83,14 +66,29 @@ for d_type in range(1, 4):
         print("Accuracy Percentage of KNN classification for test data with",
               distance_type[d_type - 1], "and k =", k_val, "is", val_accuracy)
         if val_accuracy >= accuracy:
-            accuracy = val_accuracy
-            optimal_k = k_val
             optimal_d_type = d_type
+            accuracy = val_accuracy
 print(optimal_k)
 print(optimal_d_type)
 
-# TODO ask if the accuracy same
-# TODO create confusion matrix
+validate_knn = KNN(k=optimal_k, distance_type=optimal_d_type)
+validate_knn.fit(X_train, y_train)
+y_prediction_test = validate_knn.all_prediction(X_test)
+test_accuracy = calculate_accuracy(y_test, y_prediction_test)
+print("Accuracy Percentage of KNN classification for test data with",
+      distance_type[optimal_d_type - 1], "and k =", optimal_k, "is", test_accuracy)
+
+confusion_matrix = confusion_matrix(y_test, y_prediction_test)
+print(confusion_matrix)
+
+plt.figure(figsize=(10, 6))
+ax = sns.heatmap(confusion_matrix, annot=True, cmap='Blues')
+ax.set_title('Emotions Confusion Matrix\n\n')
+ax.set_xlabel('\nPredicted Emotion Category')
+ax.set_ylabel('Actual Emotion Category ')
+
+ax.xaxis.set_ticklabels(['neutral', 'calm', 'happy', 'sad', 'angry', 'fearful', 'disgust', 'surprised'])
+ax.yaxis.set_ticklabels(['neutral', 'calm', 'happy', 'sad', 'angry', 'fearful', 'disgust', 'surprised'])
+plt.show()
+
 # TODO calculate mean squared error
-
-
